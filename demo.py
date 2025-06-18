7822d7770b35bd8ad80c3911e27c43380204db8e
Sona Rani <110620118+sonarani1@users.noreply.github.com>
2025-06-18 16:06:27 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,14 @@
 print("Hello world")
+def area_of_circle(r):
+  Argd:
+  r (float): Area of Circle
+  Returns:
+  float : Area of Circle
+    pi=3.14
+    return pi*r*r
+def main():
+  area = area_of_circle(10)  
+  print(area)
+
+if __name__ == "__main__":
+  main()
